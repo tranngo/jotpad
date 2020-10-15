48e7218e2e56cfebdef49762907152e5dc55104f
Tran Ngo <tranngo@usc.edu>
2020-10-15 14:31:43 -0700

    Created Editor.js

---
+++ src/components/Editor.js
@@ -0,0 +1,5 @@
+import React, { useEffect, useMemo, useState } from "react";
+import { createEditor } from "slate";
+import { Slate, Editable, withReact } from "slate-react";
+
+const Editor = () => {};
